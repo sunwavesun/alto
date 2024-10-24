13d898f528c54990af20d1e56648c2e789a0af91
Sun Hyuk Ahn <joycaleb9705@berkeley.edu>
2024-10-24 15:23:37 -0700

    Squashed 'harmony/safe-singleton-factory/' content from commit 57917ce
    git-subtree-dir: harmony/safe-singleton-factory
    git-subtree-split: 57917ced6b87ab9b37250c9e66e5e95608615e2d

---
+++ scripts/estimate.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import { runScript } from './utils';
+import { DeploymentEstimation, estimateDeploymentTransaction } from './common';
+
+dotenv.config()
+
+
+async function runEstimateDeploymentTransaction(): Promise<DeploymentEstimation> {
+	const rpcUrl = process.env.RPC
+	if (rpcUrl === undefined) throw "RPC environment variable must be defined"
+	return estimateDeploymentTransaction(rpcUrl)
+}
+
+runScript(runEstimateDeploymentTransaction)
